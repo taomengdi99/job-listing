6365da6d50916f53a31fe815b8fa60d7a540039d
陶梦迪 <taomengdi99@163.com>
2017-01-25 10:24:46 +0800

    add require is admin

--- app/controllers/admin/jobs_controller.rb
+++ app/controllers/admin/jobs_controller.rb
@@ -1,5 +1,13 @@
 class Admin::JobsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :update, :edit, :destroy]
+  before_action :require_is_admin
+  def require_is_admin
+    if current_user.email != 'taomengdi99@163.com'
+      flash[:alert] = 'You are not admin'
+      redirect_to root_path
+    end
+  end
+
   def index
     @jobs = Job.all
   end

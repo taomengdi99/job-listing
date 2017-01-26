e159a29a326d1a290754c5aa756b92601543c349
陶梦迪 <taomengdi99@163.com>
2017-01-26 11:28:07 +0800

    implement step 5

--- app/models/job.rb
+++ app/models/job.rb
@@ -4,4 +4,13 @@ class Job < ApplicationRecord
   validates :wage_lower_bound, presence: true
   validates :wage_upper_bound, numericality: { greater_than: 0, less_than: 100000000 }
   validates :wage_lower_bound, numericality: { greater_than: 0, less_than: 10000 }
+
+  def publish!
+    self.is_hidden = false
+    self.save
+  end
+  def hide!
+    self.is_hidden = true
+    self.save
+  end
 end

615e28cfec82c618bc46f424b82a743a85697fc0
陶梦迪 <taomengdi99@163.com>
2017-01-29 20:49:54 +0800

    better jobs/show views

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,13 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+u = User.new
+u.email = "admin@test.com"           # 可以改成自己的 email
+
+u.password = "123456"                # 最少要六码
+
+u.password_confirmation = "123456"   # 最少要六码
+
+u.is_admin = true
+u.save
+
+Job.create!([title: 'time machine maintenance personnel', description: 'in a time machine technology, completes the time machine maintenance, in case the customer through time into the time in the process of space debris.', wage_upper_bound: 50000, wage_lower_bound: 9999, is_hidden: 'false'])
+Job.create!([title: 'psychological counselor', description: "customers through the past and the future, and sometimes will be lost in time, can't distinguish is the past or the future of themselves, so we need you to have the ability to bear customer psychological problems, help them realize the reality and the past.", wage_upper_bound: 50000, wage_lower_bound: 9999, is_hidden: 'false'])
+Job.create!([title: 'customer guides', description: 'to guide customers to use, to help them to go to the point in time they want.', wage_upper_bound: 30000, wage_lower_bound: 9999, is_hidden: 'false'])
